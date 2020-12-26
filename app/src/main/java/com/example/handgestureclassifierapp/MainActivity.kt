adec750efba2b7d0888a74c01b99bf6bb795837b
Pratik_Bhangale <pratiks.bhangale@gmail.com>
2020-12-26 09:04:42 +0530

    Commit 2

--- app/src/main/java/com/example/handgestureclassifierapp/MainActivity.kt
+++ app/src/main/java/com/example/handgestureclassifierapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.handgestureclassifierapp
 
 import android.content.Intent
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.provider.MediaStore
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +22,6 @@ class MainActivity : AppCompatActivity() {
     private val mLabelPath = "labels.txt"
     private lateinit var classifier: Classifier
     private var result: Any? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +56,39 @@ class MainActivity : AppCompatActivity() {
 
     private fun showResults() {
         val resultview = findViewById<TextView>(R.id.resultsview1)
-        resultview.text = result.toString()
+        val res = result.toString()
+        resultview.text = res
+        openApps(res)
     }
 
+    private fun openApps(resulltstr:String){
+        Toast.makeText(this, "$resulltstr[16] is value", Toast.LENGTH_SHORT).show()
+
+        if (resulltstr[16] == '4'){
+            val whatsapp = packageManager.getLaunchIntentForPackage("com.whatsapp") //Intent is created to launch the app
+            if (whatsapp != null) {
+                startActivity(whatsapp)
+            } else {
+                Toast.makeText(this@MainActivity, "App not found", Toast.LENGTH_LONG).show() // If app does not exist in device, this will toast message "App not found"
+            }
+        }
+        else if (resulltstr[16] == '1'){
+            val youtubeintent = packageManager.getLaunchIntentForPackage("com.google.android.youtube")
+            if (youtubeintent != null) {
+                startActivity(youtubeintent)
+            } else {
+                Toast.makeText(this@MainActivity, "There is no package available in android", Toast.LENGTH_LONG).show()
+            }
+        }
+        else if (resulltstr[16] == '2'){
+            val message = Intent(Intent.ACTION_MAIN)
+            message.addCategory(Intent.CATEGORY_APP_MESSAGING)
+            startActivity(message)
+
+        }
+        else {
+            val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+            startActivityForResult(intent, 8293)
+        }
+    }
 }
